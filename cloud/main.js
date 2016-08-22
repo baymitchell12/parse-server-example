40a45a8243d78d9049b17c194c42a8cdb3aced4e
Baylor Mitchell <baylormitchell@gmail.com>
2016-08-21 20:29:45 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,5 @@
 
 Parse.Cloud.define('hello', function(req, res) {
   
-  res.success("req.params.firstVar");// + " " + request.params.secondVar);
+  res.success(req.params.firstVar);// + " " + request.params.secondVar);
 });
