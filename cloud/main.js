f5947da7bc3b1ce2937c46b5b0689bd003962a9f
Baylor Mitchell <baylormitchell@gmail.com>
2016-08-21 20:19:42 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -3,5 +3,5 @@ Parse.Cloud.define('hello', function(req, res) {
   
   Parse.Cloud.userMasterKey();
   
-  res.success(request.params.firstVar + " " + request.params.secondVar);
+  res.success(request.params.firstVar)// + " " + request.params.secondVar);
 });
